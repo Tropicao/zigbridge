bf02ea5664657c8770dc44b88e11d8d2f29f58af
Alexis Lothoré <alexis.lothore@gmail.com>
2017-11-28 23:17:30 +0100

    Add basic open/close to test lib

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <rpc.h>
+#include <dbgPrint.h>
 
-int main(int argc, char *argv[])
+#define SERIAL_DEVICE   "/dev/ttyACM0"
+
+int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
 {
-    fprintf(stdout, "Hello World\n");
+	int serialPortFd = rpcOpen(SERIAL_DEVICE, 0);
+	if (serialPortFd == -1)
+	{
+		dbg_print(PRINT_LEVEL_ERROR, "could not open serial port\n");
+		exit(-1);
+	}
+    dbg_print(PRINT_LEVEL_INFO, "Device %s opened\n", SERIAL_DEVICE);
+    rpcClose();
+    dbg_print(PRINT_LEVEL_INFO, "Device %s closed\n", SERIAL_DEVICE);
     exit(0);
 }
