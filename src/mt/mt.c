37e5a4f1dad60c34c7f15690967b9602f60c1f8b
Alexis Lothoré <alexis.lothore@gmail.com>
2018-01-28 17:56:51 +0100

    Re-enable UTIL module

--- src/mt/mt.c
+++ src/mt/mt.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "mt.h"
 #include "mt_sys.h"
+#include "mt_util.h"
 #include "rpc.h"
 #include "conf.h"
 
@@ -12,12 +13,14 @@ int zg_mt_init()
     int res = 0;
     res |= zg_rpc_init(zg_conf_get_znp_device_path());
     res |= zg_mt_sys_init();
+    res |= zg_mt_util_init();
 
     return res;
 }
 
 void zg_mt_shutdown()
 {
+    zg_mt_util_shutdown();
     zg_mt_sys_shutdown();
     zg_rpc_shutdown();
 }
